39cbd0a61b3da328e634ed2f31c7650b3f3d7a07
StevenDavisTechNotes <AComputing@LightLink.com>
2017-01-29 08:28:04 -0500

    Pruning unused examples

--- WebApp - Angular2/app/app.module.ts
+++ WebApp - Angular2/app/app.module.ts
@@ -6,10 +6,6 @@ import {RouterModule, Routes} from "@angular/router";
 import {AgGridModule} from "ag-grid-ng2/main";
 // application
 import {AppComponent} from "./app.component";
-// rich grid
-import {RichGridComponent} from "./rich-grid.component";
-// declarative
-import {RichGridDeclarativeComponent} from "./rich-grid-declarative.component";
 // from component
 import {FromComponentComponent} from "./from-component.component";
 import {SquareComponent} from "./square.component";
@@ -17,17 +13,6 @@ import {ParamsComponent} from "./params.component";
 import {CubeComponent} from "./cube.component";
 import {CurrencyComponent} from "./currency.component";
 
-// from rich component
-import {FromRichComponent} from "./from-rich.component";
-import {ClickableModule} from "./clickable.module";
-import {RatioModule} from "./ratio.module";
-import {RatioParentComponent} from "./ratio.parent.component";
-import {ClickableParentComponent} from "./clickable.parent.component";
-// editor
-import {EditorComponent} from "./editor-component.component";
-import {NumericEditorComponent} from "./numeric-editor.component";
-import {MoodEditorComponent} from "./mood-editor.component";
-import {MoodRendererComponent} from "./mood-renderer.component";
 // floating row
 import {WithFloatingRowComponent} from "./floating-row-renderer.component";
 import {StyledComponent} from "./styled-renderer.component";
@@ -45,19 +30,6 @@ import {MasterComponent} from "./masterdetail-master.component";
 import {DetailPanelComponent} from "./detail-panel.component";
 
 const appRoutes: Routes = [
-    {path: 'rich-grid', component: RichGridComponent, data: {title: "Rich Grid with Pure JavaScript"}},
-    {
-        path: 'rich-grid-declarative',
-        component: RichGridDeclarativeComponent,
-        data: {title: "Rich Grid with Declarative Markup"}
-    },
-    {path: 'from-component', component: FromComponentComponent, data: {title: "Using Dynamic Components"}},
-    {path: 'from-rich-component', component: FromRichComponent, data: {title: "Using Dynamic Components - Richer Example"}},
-    {path: 'editor-component', component: EditorComponent, data: {title: "Using Cell Editor Components"}},
-    {path: 'floating-row', component: WithFloatingRowComponent, data: {title: "Using Floating Row Renderers"}},
-    {path: 'full-width', component: WithFullWidthComponent, data: {title: "Using Full Width Renderers"}},
-    {path: 'group-row', component: WithGroupRowComponent, data: {title: "Using Group Row Renderers"}},
-    {path: 'filter', component: FilterComponentComponent, data: {title: "With Filters Components"}},
     {path: 'master-detail', component: MasterComponent, data: {title: "Master Detail Example"}},
     {path: '', redirectTo: 'master-detail', pathMatch: 'full'}
 ];
@@ -73,34 +45,20 @@ const appRoutes: Routes = [
                 CubeComponent,
                 ParamsComponent,
                 CurrencyComponent,
-                RatioParentComponent,
-                ClickableParentComponent,
-                NumericEditorComponent,
-                MoodEditorComponent,
-                MoodRendererComponent,
                 StyledComponent,
                 NameAndAgeRendererComponent,
                 MedalRendererComponent,
                 PartialMatchFilterComponent,
                 DetailPanelComponent
-            ]),
-        RatioModule,
-        ClickableModule
+            ])
     ],
     declarations: [
         AppComponent,
-        RichGridComponent,
-        RichGridDeclarativeComponent,
         FromComponentComponent,
         SquareComponent,
         CubeComponent,
         ParamsComponent,
         CurrencyComponent,
-        FromRichComponent,
-        EditorComponent,
-        NumericEditorComponent,
-        MoodEditorComponent,
-        MoodRendererComponent,
         WithFloatingRowComponent,
         StyledComponent,
         WithFullWidthComponent,
